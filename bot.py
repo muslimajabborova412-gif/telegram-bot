6c1f740ac07ba13f29972997cb59ad2ee4f2c0e5
muslimajabborova412-gif <muslimajabborova412@gmail.com>
2026-06-19 16:57:57 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,16 +2,16 @@ import os
 import asyncio
 from flask import Flask, request
 from telegram import Bot, Update
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
+from telegram.ext import ApplicationBuilder
 
 TOKEN = "8201016798:AAEwG4rrqu-9o1H-wOdVzSr6WPZal_6_7N0"
 app = Flask(__name__)
 
-# Сохтани Loop барои Render
+# Истифодаи loop-и нав барои Render
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 
-# Танзими бот
+# Ботро омода мекунем
 app_bot = ApplicationBuilder().token(TOKEN).build()
 
 @app.route('/webhook', methods=['POST'])
@@ -28,5 +28,5 @@ if __name__ == '__main__':
     webhook_url = "https://telegram-bot-9thf.onrender.com/webhook"
     loop.run_until_complete(bot.set_webhook(url=webhook_url))
     
-    # Оғози сервер
-    app.run(host='0.0.0.0', port=10000)
+    # Оғози сервер дар порту 10000
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
