cd9f7fd37f07d58b3cd76ef59e1e816789fd2331
muslimajabborova412-gif <muslimajabborova412@gmail.com>
2026-07-10 22:27:53 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 from config import BOT_TOKEN
 from database import init_db
 from handlers import start_command, button_callback_handler, text_handler
@@ -8,15 +8,15 @@ def main():
     # 1. Сар кардани Базаи маълумот
     init_db()
     
-    # 2. Сохтани лоиҳаи Бот (бе job_queue барои пешгирии хатогӣ)
-    application = Application.builder().token(BOT_TOKEN).job_queue(None).build()
+    # 2. Сохтани лоиҳаи Бот бо JobQueue
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
     
-    # 3. Пайваст кардани командаҳо ва тугмаҳо (Handlers)
+    # 3. Пайваст кардани командаҳо ва тугмаҳо
     application.add_handler(CommandHandler("start", start_command))
     application.add_handler(CallbackQueryHandler(button_callback_handler, pattern="^check_subscription$"))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_handler))
     
-    # 4. Фаъол кардани автопостинги ҳаррӯза
+    # 4. Фаъол кардани автопостинг
     start_scheduler(application)
     
     # 5. Ба кор андохтани бот
