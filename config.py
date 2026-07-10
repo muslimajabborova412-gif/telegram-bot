387394cb059355dbf9dde6183a8811f072ac5c63
muslimajabborova412-gif <muslimajabborova412@gmail.com>
2026-07-10 21:50:55 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+import os
 
+# Токени бот аз Environment Variables-и Render гирифта мешавад
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# ID ё Юзернейми канали шумо (ҳатман бо @ нависед)
+CHANNEL_USERNAME = "@English_Books_send"
+
+# ID-и худи шумо (чун Админ). Рақами ID-и худро ба ҷои 123456789 нависед.
+# Агар ID-атонро надонед, дар Telegram ба боти @userinfobot "салом" нависед, он ID-и шуморо медиҳад.
+ADMIN_ID = 123456789  # Инашро ба ID-и худатон иваз кунед
