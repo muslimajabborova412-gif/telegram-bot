1a30ee3219327c404e27f8a07d281dd7e022edce
muslimajabborova412-gif <muslimajabborova412@gmail.com>
2026-07-11 07:17:19 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
 import os
 
-# Токени бот аз Environment Variables-и Render гирифта мешавад
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-# ID ё Юзернейми канали шумо (ҳатман бо @ нависед)
+BOT_TOKEN = os.getenv("BOT_TOKEN") # Инро дар Render дар қисми Environment гузоред
 CHANNEL_USERNAME = "@English_Books_send"
-
-# ID-и худи шумо (чун Админ).
-ADMIN_ID = 6987970875  # Рақами ID-и худро дар ин ҷо нависед
+ADMIN_ID = 6987970875 # ID-и худро гузоред
